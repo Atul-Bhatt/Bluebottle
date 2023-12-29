f947944417fa5de9b69f1dc676a1d9b704c46813
Atul Bhatt <atulbhatt61@gmail.com>
2023-12-29 15:10:00 +0530

    handling more commands

--- src/server.rs
+++ src/server.rs
@@ -30,7 +30,27 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                                  "USER" => {
                                      // Handler USER command (e.g, authenticate user)
                                      println!("Received USER command");
-                                     socket.write_all(b"331 Username OK").await.unwrap();
+                                     socket.write_all(b"331 Username OK\r\n").await.unwrap();
+                                 }
+                                 "PASS" => {
+                                     // Verify Password
+                                     println!("Received PASS command");
+                                     socket.write_all(b"230 User logged in\r\n").await.unwrap();
+                                 }
+                                 "CWD" => {
+                                     // Change working directory
+                                     println!("Received CWD command");
+                                     socket.write_all(b"250 Working directory changed\r\n").await.unwrap();
+                                 }
+                                 "RETR" => {
+                                     // Retrieve the copy of a file
+                                     println!("Received RETR command");
+                                     socket.write_all(b"250 Retrieving file\r\n").await.unwrap();
+                                 }
+                                 "PORT" => {
+                                     // Address and port to which server should connect
+                                     println!("Received PORT command");
+                                     socket.write_all(b"250 Connecting to port\n\r").await.unwrap();
                                  }
                                  _ => {
                                      println!("Unknown command:{}", command);
